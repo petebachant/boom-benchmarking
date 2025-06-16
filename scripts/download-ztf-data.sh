56284dbc70ec08797ff6f0bed518d68e6199587a
Pete Bachant <petebachant@gmail.com>
2025-06-16 15:43:55 -0700

    Start pipeline

---
+++ scripts/download-ztf-data.sh
@@ -0,0 +1,18 @@
+mkdir -p data
+
+FNAME=ztf_public_20250614.tar.gz
+
+# Only download the file if it does not already exist
+if [ -f data/${FNAME} ]; then
+    echo "File data/${FNAME} already exists; Skipping download"
+else
+    echo "Downloading ZTF data to data/${FNAME}"
+    wget -O data/${FNAME} https://ztf.uw.edu/alerts/public/${FNAME}
+fi
+
+# Data directory is the file name without extension
+FNAME_NO_EXT=${FNAME%.tar.gz}
+
+mkdir -p data/${FNAME_NO_EXT}
+
+tar -xzf data/${FNAME} -C data/${FNAME_NO_EXT}
